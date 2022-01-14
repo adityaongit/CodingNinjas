310a95e450f457cb98b469bee2ced7aefbf64c74
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-14 20:10:32 +0530

    Palindrome function added

--- Recursion/Recursion Revision/src/OperationOnDigits.java
+++ Recursion/Recursion Revision/src/OperationOnDigits.java
@@ -1,6 +1,6 @@
 public class OperationOnDigits {
     public static void main(String[] args) {
-        reverse(1342);
+        System.out.println(isPalindrome(1342));
     }
 
     static int sumOfDigits(int n) {
@@ -17,7 +17,7 @@ public class OperationOnDigits {
         return (n % 10) * productOfDigits(n / 10);
     }
 
-    static void reverse(int n) {
+    static void reverseVoid(int n) {
         if (n < 10) {
             System.out.println(n);
         } else {
@@ -25,4 +25,22 @@ public class OperationOnDigits {
             reverse(n / 10);
         }
     }
+
+    static int reverse(int num) {
+        int ans = 0;
+        if (num == 0) {
+            return ans;
+        }
+        if (num > 0) {
+            int temp = num % 10;
+            ans = ans * 10 + temp;
+            reverse(num / 10);
+        }
+        return ans;
+    }
+
+    static boolean isPalindrome(int n) {
+        return n == reverse(n);
+    }
+
 }
