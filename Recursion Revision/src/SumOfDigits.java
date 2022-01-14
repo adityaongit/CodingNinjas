fbd0a38f2185ddca564b9a91cf69617d4a8ca827
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-14 15:07:12 +0530

    Sum of Digits 1 added

--- Recursion Revision/src/SumOfDigits.java
+++ Recursion Revision/src/SumOfDigits.java
@@ -1,3 +1,12 @@
 public class SumOfDigits {
-    
+    public static void main(String[] args) {
+        int res = sumOfDigits(1342);
+        System.out.println(res);
+    }
+    static int sumOfDigits(int n){
+        if(n/10 == 0){
+            return 1;
+        }
+        return (n%10)+sumOfDigits(n/10);
+    }
 }
