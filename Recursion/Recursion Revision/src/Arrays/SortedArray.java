ef65d26ade89ce524e323da084d0887e9320bd3c
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-15 19:22:05 +0530

    isSorted function added

---
+++ Recursion/Recursion Revision/src/Arrays/SortedArray.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 8, 5, 6 };
+        System.out.println(isSorted(arr, 0));
+    }
+
+    static boolean isSorted(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return true;
+        }
+        return arr[index] < arr[index + 1] && isSorted(arr, index + 1);
+
+    }
+}
