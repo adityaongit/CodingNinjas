6dc14fb051a8de5b8111824903941ab0d8237817
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-15 20:51:34 +0530

    Linear Seach updated

--- Recursion/Recursion Revision/src/Arrays/LinearSearch.java
+++ Recursion/Recursion Revision/src/Arrays/LinearSearch.java
@@ -1,9 +1,12 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 8, 5, 6 };
-        System.out.println(findIndex(arr, 8, 0));
+        int[] arr = { 1, 2, 3, 8, 3, 5, 6 };
+        findAllIndex(arr, 3, 0);
+        System.out.println(list);
     }
 
     static boolean find(int[] arr, int target, int index) {
@@ -22,5 +25,17 @@ public class LinearSearch {
         } else {
             return findIndex(arr, target, index + 1);
         }
+    }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
     }
 }
