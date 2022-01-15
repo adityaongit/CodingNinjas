7f03286f3a37ec60ba6ef4b2cbe79be046435c20
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-15 20:45:14 +0530

    Linear Seach added

---
+++ Recursion/Recursion Revision/src/Arrays/LinearSearch.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 8, 5, 6 };
+        System.out.println(findIndex(arr, 8, 0));
+    }
+
+    static boolean find(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return false;
+        }
+        return arr[index] == target || find(arr, target, index + 1);
+    }
+
+    static int findIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return -1;
+        }
+        if (arr[index] == target) {
+            return index;
+        } else {
+            return findIndex(arr, target, index + 1);
+        }
+    }
+}
