124e59fae1292338dcaf3a8af42230d9ed6a7660
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-21 21:40:45 +0530

    updated

--- OOPS/src/generics/PairUse.java
+++ OOPS/src/generics/PairUse.java
@@ -4,5 +4,8 @@ public class PairUse {
 	public static void main(String[] args) {
 		Pair<String> p = new Pair<String>("ab", "cd");
 		p.setFirst("def");
+		String s = p.getFirst();
+
+		Pair p1 = new Pair(1,2);
 	}
 }
