a36bde9bd503064ca6856269d709e9e97bc2223d
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-25 22:01:25 +0530

    function created

--- Backtracking/RatInMaze.java
+++ Backtracking/RatInMaze.java
@@ -1,3 +1,17 @@
 public class RatInMaze {
-    
+    public static void main(String[] args) {
+        int maze[][] = { { 1, 1, 0 }, { 1, 1, 0 }, { 1, 1, 1 } };
+        boolean pathPossible = ratInMaze(maze);
+        System.out.println(pathPossible);
+    }
+
+    public static boolean ratInMaze(int[][] maze) {
+        int n = maze.length;
+        int path[][] = new int[n][n];
+        return solveMaze(maze, 0, 0, path);
+    }
+
+    public static boolean solveMaze(int maze[][], int i, int j, int path[][]) {
+        return false;
+    }
 }
