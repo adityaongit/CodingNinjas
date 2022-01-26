c27bd15674a76e4da1b4d30dff6b2f5750e834c2
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-26 22:41:20 +0530

    n queen problem added

---
+++ Backtracking/NQueens.java
@@ -0,0 +1,17 @@
+public class NQueens {
+    public static void main(String[] args) {
+
+    }
+
+    public static void placeNQueens(int n) {
+        int board[][] = new int[n][n];
+        placeQueens(n,0);
+    }
+
+    private static void placeQueens(int n, int row) {
+        if(row==n){
+            // Valid Board Configuration
+        }
+        
+    }
+}
