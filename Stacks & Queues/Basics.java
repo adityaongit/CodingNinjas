20bc62a734795b6242a36e345e51a6a76989cf44
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-28 23:44:40 +0530

    operation tested

--- Stacks & Queues/Basics.java
+++ Stacks & Queues/Basics.java
@@ -5,21 +5,21 @@ public class Basics {
         Stack<Integer> st = new Stack<>();
 
         st.push(10);
-        System.out.println(st + "->" + st.peek() + " " + st.size());
+        System.out.println(st + " -> " + st.peek() + " " + st.size());
         st.push(20);
-        System.out.println(st + "->" + st.peek() + " " + st.size());
+        System.out.println(st + " -> " + st.peek() + " " + st.size());
         st.push(30);
-        System.out.println(st + "->" + st.peek() + " " + st.size());
+        System.out.println(st + " -> " + st.peek() + " " + st.size());
         st.push(40);
-        System.out.println(st + "->" + st.peek() + " " + st.size());
+        System.out.println(st + " -> " + st.peek() + " " + st.size());
         st.pop();
-        System.out.println(st + "->" + st.peek() + " " + st.size());
+        System.out.println(st + " -> " + st.peek() + " " + st.size());
         st.pop();
-        System.out.println(st + "->" + st.peek() + " " + st.size());
+        System.out.println(st + " -> " + st.peek() + " " + st.size());
         st.pop();
-        System.out.println(st + "->" + st.peek() + " " + st.size());
+        System.out.println(st + " -> " + st.peek() + " " + st.size());
         st.pop();
-        System.out.println(st + "->" + st.size());
+        System.out.println(st + " -> " + st.size());
 
     }
 }
