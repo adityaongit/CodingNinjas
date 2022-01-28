754f174f62f3a24ff7b2653fe11ad63325f95018
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-28 12:24:09 +0530

    syntax correction

--- Backtracking/NQueens.java
+++ Backtracking/NQueens.java
@@ -1,12 +1,14 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class NQueens {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
         int[][] chess = new int[n][n];
         printNQueens(chess, "", 0);
+
+        scn.close();
     }
 
     public static void printNQueens(int[][] chess, String qsf, int row) {
@@ -15,18 +17,15 @@ public class NQueens {
             return;
         }
         for (int col = 0; col < chess.length; col++) {
-            if (chess[row][col] == 0
-                    && isQueenSafe(chess, row, col) == true) {
+            if (isQueenSafe(chess, row, col) == true) {
                 chess[row][col] = 1;
-                printNQueens(chess,
-                        qsf + row + "-" + col + ", ", row + 1);
+                printNQueens(chess, qsf + row + "-" + col + ", ", row + 1);
                 chess[row][col] = 0;
             }
         }
     }
 
-    public static boolean isQueenSafe(int[][] chess,
-            int row, int col) {
+    public static boolean isQueenSafe(int[][] chess, int row, int col) {
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (chess[i][j] == 1) {
                 return false;
@@ -39,14 +38,7 @@ public class NQueens {
             }
         }
 
-        for (int i = row - 1, j = col + 1; i >= 0
-                && j < chess.length; i--, j++) {
-            if (chess[i][j] == 1) {
-                return false;
-            }
-        }
-
-        for (int i = row, j = col - 1; j >= 0; j--) {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
             if (chess[i][j] == 1) {
                 return false;
             }
