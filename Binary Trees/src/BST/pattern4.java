6062108d8d45f4dc7de8000e38130995cc2c1f8d
Aditya Jindal <raghavjindal017@gmail.com>
2022-02-18 11:15:36 +0530

    dir deleted

---
+++ Binary Trees/src/BST/pattern4.java
@@ -0,0 +1,30 @@
+package BST;
+
+import java.util.Scanner;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+	
+		{
+			  Scanner scn = new Scanner(System.in);
+
+		      int n = scn.nextInt();
+		      int space=n-1;
+		      int star = 1;
+		      for (int i = 1;i<=n;i++){
+		          for (int j =1;j<=space;j++){
+		              System.out.print("\t");
+		          }
+		          for ( int j =1; j<=star;j++){
+		              System.out.print("*\t");
+		          }
+		          space--;
+		          star++;
+		          System.out.println();
+		      }
+		   }
+		}
+
+}
+
